061a5f92bac8ae6fe9e31f24246e256afcb4d71c
hyeri <hyerijung0903@gmail.com>
2021-06-07 23:49:03 +0900

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[101] = { 0 };
+
+//재귀함수 이용
+int factorial(int n) {
+	if (n == 1)
+		return 1;
+	else
+		return n * factorial(n - 1);
+}
+
+//반복문 이용
+void factorial2(int n) {
+	int result = 1;
+	for (int i = 1; i <= n; i++) {
+		result = result * i;
+	}
+	cout << result << endl;
+}
+
+//dp(memorize) 이용
+int factorial3(int n) {
+	if (dp[n] != 0) {
+		return dp[n];
+	}
+	else if (n <= 1) {
+		return 1;
+	}
+	else {
+		dp[n] = n * factorial3(n - 1);
+		return dp[n];
+	}
+
+	return n * factorial3(n - 1);
+}
+
+int main()
+{
+	//cout << factorial(5);
+	//factorial2(5);
+	cout << factorial3(5);
+	return 0;
+}
