0491eeede202f233d0bd9c208fa55fa63444f682
hyeri <hyerijung0903@gmail.com>
2021-06-11 10:24:29 +0900

    Create mergeSort.py

---
+++ mergeSort.py
@@ -0,0 +1,28 @@
+#합병정렬
+#평균시간복잡도 : nlogn
+
+def merge(left, right):
+    tmp = []
+    i = j = 0
+    while i<len(left) and j < len(right):
+        if left[i] <= right[j]:
+            tmp.append(left[i])
+            i+=1
+        else:
+            tmp.append(right[j])
+            j += 1
+    if i < len(left):
+        tmp += left[i:len(left)]
+    if j < len(right):
+        tmp += right[j:len(right)]
+    return tmp
+        
+            
+def mergeSort(li):
+    if len(li) <= 1:
+        return li
+
+    mid = len(li) // 2
+    left = mergeSort(li[:mid])
+    right = mergeSort(li[mid:])
+    return merge(left, right)
